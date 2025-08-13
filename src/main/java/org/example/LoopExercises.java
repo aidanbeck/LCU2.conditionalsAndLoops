43df742cae26863b88ea5e2f72bd3bd223647617
aidanbeck <aidanmbeck@outlook.com>
2025-08-12 20:31:07 -0500

    assignment complete

--- src/main/java/org/example/LoopExercises.java
+++ src/main/java/org/example/LoopExercises.java
@@ -3,14 +3,22 @@ package org.example;
 public class LoopExercises {
     public int sum(int n) {
         // Replace the line below with code that returns the sum of the numbers from 1 to n
-        // (use a for loop)
-        return 0;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
     }
 
     public int sumUntilEven(int n) {
         // Replace the line below with code that returns the sum of the numbers from 1 to n
         // but stops adding when the sum is even
-        // (use a while loop with a sum variable and a counter variable)
-        return 0;
+        int sum = 0;
+        int i = 1;
+        while (i <= n && (sum % 2 != 0 || sum == 0)) {
+            sum += i;
+            i++;
+        }
+        return sum;
     }
 }
